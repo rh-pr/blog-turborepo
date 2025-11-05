29f8d810258149011cd086e7e8a28648c891adfc
Ruslana <ruslana.hural.j@gmail.com>
2025-11-05 19:45:26 +0100

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //todo: change for deployment
+  app.enableCors({
+    origin: 'http://localhost:3000', // ✅ allow your frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // if you need cookies
+  });
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT ?? 8000);
 }
